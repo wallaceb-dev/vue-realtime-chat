6bee4ea2710940ffc7b14535256fcd66b5fe36a9
Wallace B. Teixeira <wallaceb.teixeira@gmail.com>
2022-04-12 21:17:33 -0300

    feat! add message components

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="chat container">
-    <h2>Chat {{ this.name }}</h2>
+    <h2 class="center teal-text">Ninja Chat</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages">
+          <li>
+            <span class="teal-text">Name</span>
+            <span class="grey-text text-darken-3">message</span>
+            <span class="grey-text time">time</span>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <NewMessage :name="name" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import NewMessage from "@/components/NewMessage";
+
 export default {
   name: 'Chat',
   props: ['name'],
+  components: {
+    NewMessage
+  },
   data() {
     return {};
   },
@@ -15,4 +34,15 @@ export default {
 </script>
 
 <style scoped>
+.chat h2 {
+  font-size: 2.6em;
+  margin-bottom: 40px;
+}
+.chat span {
+  font-size: 1.4em;
+}
+.chat .time {
+  display: block;
+  font-size: 1.2em ;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,11 @@ export default new Router({
       component: Chat,
       props: true,
       beforeEnter: (to, from, next) => {
-        console.log(to.params.name);
+        if (to.params.name) {
+          next();
+        } else {
+          next({ name: 'Welcome' });
+        }
       }
     }
   ]
